b71e451ddd9a6495db3a90778b7fcb40b1a672a3
MarcellinoNatanael <marcellinonatanael74@gmail.com>
2026-05-31 15:43:50 +0700

    fix: update batasDaftar to 2026-06-01T11:59:00

--- js/daftar.js
+++ js/daftar.js
@@ -10,7 +10,7 @@
 const EVENT_CONFIG = {
   tanggal      : new Date('2026-06-01T19:00:00'),
   gmeetLink    : 'https://meet.google.com/zdd-xoiv-zcp',
-  batasDaftar  : new Date('2026-05-31T23:59:00'),
+  batasDaftar  : new Date('2026-06-1T11:59:00'),
   kuota        : 50,
   judul        : 'Break The Cycle of Addiction with Smart Gaming Habits',
   deskripsi    : 'Webinar edukasi yang membahas tentang bahaya gaming addiction, cara mengenali tanda-tandanya, dan strategi gaming yang sehat berbasis pendekatan psikologi.',
